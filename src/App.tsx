158c4a5fb6e3e09df254cb3faebd1ad688ba11f9
LicChamoy <diego78041@gmail.com>
2025-08-08 13:17:39 -0600

    Dice don gato que siempre no

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './pages/Hero';
 import Backend from './pages/BackendOverview';
 import ThreeD from './pages/ThreeDOverview';
 import Video from './pages/VideoOverview';
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<PageWrapper><Landing /></PageWrapper>} />
-        <Route path="/hero" element={<Hero />} />
         <Route path="/backend" element={<PageWrapper><Backend /></PageWrapper>} />
         <Route path="/3d" element={<PageWrapper><ThreeD /></PageWrapper>} />
         <Route path="/video" element={<PageWrapper><Video /></PageWrapper>} />
